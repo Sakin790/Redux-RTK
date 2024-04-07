85ff034a82b03d2eec2fb21a1b3c8b40d421f3fe
Sakin790 <sakinislam79@gmail.com>
2024-04-07 12:58:26 +0600

    dr

--- 04_ReduxToolkit/src/feature/todo/todoSlice.js
+++ 04_ReduxToolkit/src/feature/todo/todoSlice.js
@@ -1,1 +1,13 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initaialState = {
+  todos: [{ id: 1, text: "Hello world" }],
+};
+
+export const todoSlice = createSlice({
+  name: "todos",
+  initaialState,
+  reducers: {
+    
+  }
+});
